3ccc3143b9c317aeaa25bbb1ca0675d2f80db794
Njengele <sakhiwo07@gmail.com>
2026-05-15 08:53:56 +0200

    Update print statement

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
 FleetWise SA: Should I Build an Uber Fleet?
 A Data Science Portfolio Project
-Author: Ngobe | ALX Africa Data Science Certification 2024
+Author: Ngobe | ALX Africa Data Science Certification
 """
 
 import streamlit as st
